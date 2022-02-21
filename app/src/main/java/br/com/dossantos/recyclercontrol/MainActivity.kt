e9458b257488f56fd95c4bb2ade9a69beb8a518d
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2022-02-21 19:03:29 -0300

    init

---
+++ app/src/main/java/br/com/dossantos/recyclercontrol/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.dossantos.recyclercontrol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
